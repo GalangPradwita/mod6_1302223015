bf415af344e41d04032325e483c6bee94cc214e1
@GalangPradwita <mgalangpradwita16@gmail.com>
2024-03-30 07:51:32 +0700

    first commit

---
+++ mod6_1302223015/mod6_1302223015/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace mod6_1302223015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
